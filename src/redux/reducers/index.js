6b5376b3595e9ab06e5db5f44bcf1dbdeeb863d1
AndreyPechterev <andreypechterev1234@mail.ru>
2023-03-28 20:58:57 +0300

    change github

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,8 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers } from "redux";
 import filters from "./filters";
 import pizzas from "./pizzas";
 import cart from "./cart";
 
-const rootReducer = combineReducers({pizzas, filters, cart});
+const rootReducer = combineReducers({ pizzas, filters, cart });
 
-export default rootReducer
+export default rootReducer;
